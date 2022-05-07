509f5e282fa55155eb904eba3b67f700fba783b4
gourav017 <gouravsahu696@gmail.com>
2022-05-07 17:46:20 +0530

    updated home page final

--- Script/gauravIndex.js
+++ Script/gauravIndex.js
@@ -1,3 +1,3 @@
 import { vikasHead, footer } from "../components/header.js";
 document.getElementById("_header").innerHTML = vikasHead();
-document.getElementById("footer").innerHTML = footer();
+document.getElementById("footerr").innerHTML = footer();
